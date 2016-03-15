747647ae820658be93e24bceb5db01b12dff126c
EPiC-APOC <server_root@hotmail.com>
2016-03-15 11:05:12 +0100

    *updates*
    Signed-off-by: EPiC

--- script.xvbmc.upper/XvBMC.py
+++ script.xvbmc.upper/XvBMC.py
@@ -1,28 +1,28 @@
 '''
     IF you copy/paste 'script.xvbmc.upper' please keep the credits -2- EPiC -4- XvBMC, Thx.
 '''
-
+ 
 import xbmc, xbmcgui
 import shutil
 import urllib2,urllib
 import os
 import xbmcaddon
-
+ 
 if sys.version_info >=  (2, 7):
     import json as json
 else:
     import simplejson as json 
-
+ 
 ########################################################################
 __ScriptName__ = "script.XvBMC.upper"
 __ScriptVersion__ = "2.0.0"
 __Author__ = "EPiC"
 __Website__ = "http://WatBenJeDan.nl/"
 ########################################################################
-
+ 
 # Set the addon environment
 addon = xbmcaddon.Addon('script.xvbmc.upper')
-
+ 
 # Parse command parameters
 try:
     # parse sys.argv for params
@@ -33,9 +33,9 @@ try:
 except:
     # no params passed
     params = {}   
-
-	
-
+ 
+  
+   
 def showMenu():
     '''Set up our main menu.'''
     
@@ -43,6 +43,7 @@ def showMenu():
     userchoice = []
     userchoice.append("Isengard Upgrade")
     userchoice.append("Jarvis Upgrade")
+    userchoice.append("Beta OE8 Upgrade")
     userchoice.append("EPiC Tweak")
     userchoice.append("Exit")
     
@@ -58,18 +59,22 @@ def showMenu():
     # Jarvis Upgrade
     elif userchoice[inputchoice] == "Jarvis Upgrade":
         JarvisUpgrade()
+
+    # BETA's Upgrade
+    elif userchoice[inputchoice] == "Beta OE8 Upgrade":
+        BETAupgrade()
  
     # Edit user preferences
     elif userchoice[inputchoice] == "EPiC Tweak":
         xbmcgui.Dialog().ok("XvBMC Nederland", "EPiC Tweaking bitches", "Coming soon to a theater near you ;-P")
-
-
-
+ 
+  
+   
 def DownloaderClass(url,dest):
     dp = xbmcgui.DialogProgress()
     dp.create('XvBMC EPiC OpenELEC Upgrade','XvBMC Kodi upgrade downloaden','')
     urllib.urlretrieve(url,dest,lambda nb, bs, fs, url=url: _pbhook(nb,bs,fs,url,dp))
-
+ 
 def _pbhook(numblocks, blocksize, filesize, url=None,dp=None):
     try:
         percent = min((numblocks*blocksize*100)/filesize, 100)
@@ -81,8 +86,8 @@ def _pbhook(numblocks, blocksize, filesize, url=None,dp=None):
     if dp.iscanceled(): 
         print 'Download Geannuleerd' # does it break, or does it not break, that is the question :-P
         dp.close()
-
-
+ 
+ 
 class IsengardClass(xbmcgui.Window):
   def __init__(self):
     dialog = xbmcgui.Dialog()
@@ -96,7 +101,7 @@ class IsengardClass(xbmcgui.Window):
    	xbmc.executebuiltin("ReloadKeymaps")
    	xbmc.executebuiltin("ReloadSkin()")
    	xbmc.executebuiltin("Notification(XvBMC EPiC OpenELEC Upgrade,EPiC Isengard upgrade geslaagd,5000,special://skin/icon.png)")
-
+ 
 class JarvisClass(xbmcgui.Window):
   def __init__(self):
     dialog = xbmcgui.Dialog()
@@ -110,9 +115,23 @@ class JarvisClass(xbmcgui.Window):
    	xbmc.executebuiltin("ReloadKeymaps")
    	xbmc.executebuiltin("ReloadSkin()")
    	xbmc.executebuiltin("Notification(XvBMC EPiC OpenELEC Upgrade,EPiC Jarvis upgrade geslaagd,5000,special://skin/icon.png)")
-
-
-
+ 
+class BetaClass(xbmcgui.Window):
+  def __init__(self):
+    dialog = xbmcgui.Dialog()
+    if dialog.yesno('EPiC XvBMC Upgrade','Upgrade -2- OE8 BETA'):
+        url = 'http://milhouse.openelec.tv/builds/master/RPi2/OpenELEC-RPi2.arm-8.0-Milhouse-20160314220202-%230314-g0026734.tar'
+        path = xbmc.translatePath(os.path.join('/storage/.update/','')) # OpenELEC   (XvBMC Nederland : https://www.facebook.com/groups/XbmcVoorBeginnersRaspberryPi/)
+    #   path = xbmc.translatePath(os.path.join('special://home',''))    # Standalone (https://www.facebook.com/groups/XvBMCnederland/)
+        lib=os.path.join(path, 'betas.tar')
+        DownloaderClass(url,lib)
+     
+   	xbmc.executebuiltin("ReloadKeymaps")
+   	xbmc.executebuiltin("ReloadSkin()")
+   	xbmc.executebuiltin("Notification(XvBMC EPiC OpenELEC Upgrade,EPiC OE8 upgrade geslaagd,5000,special://skin/icon.png)")
+   
+  
+ 
 def IsengardUpgrade():
     mydisplay = IsengardClass()
     del mydisplay
@@ -120,11 +139,15 @@ def IsengardUpgrade():
 def JarvisUpgrade():
     mydisplay = JarvisClass()
     del mydisplay
-
-
-
+ 
+def BETAupgrade():
+    mydisplay = BetaClass()
+    del mydisplay
+ 
+  
+   
 ########################################################################
 # This is where we start!
 ########################################################################
-
+ 
 showMenu()
