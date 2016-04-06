4052c11eb3f592ef28a7b359ccaaf898d6339088
EPiC-APOC <server_root@hotmail.com>
2016-04-06 10:42:36 +0200

    update
    Signed-off-by: EPiC

--- script.xvbmc.update/firmware.sh
+++ script.xvbmc.update/firmware.sh
@@ -9,9 +9,9 @@ curl https://raw.githubusercontent.com/raspberrypi/firmware/master/boot/bootcode
 curl https://raw.githubusercontent.com/raspberrypi/firmware/master/boot/start_x.elf -o /flash/start.elf
 curl https://raw.githubusercontent.com/raspberrypi/firmware/master/boot/fixup_x.dat -o /flash/fixup.dat
 
-# cp -rav /storage/.kodi/addons/sscript.xvbmc.update/Update.sh /storage/.kodi/userdata/scripts/firmware.sh       # duplicate script -2- /scripts/ #
+# cp -rav /storage/.kodi/addons/script.xvbmc.update/firmware.sh /storage/.kodi/userdata/scripts/firmware.sh    #  duplicate script -2- /scripts/  #
 
-kodi-send -a "Notification(XvBMC Firmware Update,FINISHED! PLEASE REBOOT...,4000,special://home/addons/script.xvbmc.update/icon.png)"
+kodi-send -a "Notification(XvBMC Firmware flashed,REBOOT in 5 seconds...,5000,special://home/addons/script.xvbmc.update/icon.png)"
 
 sleep 1
 reboot
