c202ff18df1df9fbbe7a882efc8a07382bfb1cc7
EPiC-APOC <Server_ROOT@hotmail.com>
2016-04-08 19:02:36 +0200

    update

---
+++ script.xvbmc.update/resources/firmwareimage.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# kodi-send -a "Notification(XvBMC Firmware flash,Preparing and auto.Reboot when done...,5000,special://home/addons/script.xvbmc.update/icon.png)"
+
+mount -o remount,rw /flash/
+# cp -rav /storage/.kodi/addons/script.xvbmc.update/resources/bin/config-turbo.txt /flash/config.txt
+ 
+curl https://raw.githubusercontent.com/XvBMC/repository.xvbmc/master/zips/update/firmware/xvbmcRC4/bootcode.bin -o /flash/bootcode.bin
+curl https://raw.githubusercontent.com/XvBMC/repository.xvbmc/master/zips/update/firmware/xvbmcRC4/fixup_x.dat -o /flash/fixup.dat
+curl https://raw.githubusercontent.com/XvBMC/repository.xvbmc/master/zips/update/firmware/xvbmcRC4/start_x.elf -o /flash/start.elf
+ 
+kodi-send -a "Notification(XvBMC default Firmware re-flashed,REBOOT in 5 seconds...,5000,special://home/addons/script.xvbmc.update/icon.png)"
+ 
+sleep 1
+reboot
