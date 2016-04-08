f63978474dc4f17045d60a492dfed0634636e507
EPiC-APOC <Server_ROOT@hotmail.com>
2016-04-08 19:00:43 +0200

    cleanup
    Signed-off-by: EPiC

--- script.xvbmc.update/resources/config1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mount -o remount,rw /flash/
-cp -rav /storage/.kodi/addons/script.xvbmc.update/resources/bin/config-turbo.txt /flash/config.txt
- 
-kodi-send -a "Notification(XvBMC NL Turbo-overclock Pi,FINISHED! PLEASE REBOOT...,4000,special://home/addons/script.xvbmc.update/icon.png)"
- 
-sleep 1
-# reboot
--- script.xvbmc.update/resources/config2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mount -o remount,rw /flash/
-cp -rav /storage/.kodi/addons/script.xvbmc.update/resources/bin/config-x265.txt /flash/config.txt
- 
-kodi-send -a "Notification(XvBMC NL x265-overclock Pi,FINISHED! PLEASE REBOOT...,4000,special://home/addons/script.xvbmc.update/icon.png)"
- 
-sleep 1
-# reboot
