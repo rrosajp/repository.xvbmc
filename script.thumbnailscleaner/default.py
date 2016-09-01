88837a0cdb2cd6e9719f232ea0895a13a76bf6dc
EPiC-APOC <server_root@hotmail.com>
2016-09-01 15:45:56 +0200

    add: script.thumbnailscleaner v1.1.5

---
+++ script.thumbnailscleaner/default.py
@@ -0,0 +1,24 @@
+﻿# -*- coding: utf-8 -*-
+
+##################################
+# Thumbnails Cleaner             #
+# by Max (m4x1m) Headroom, zeppy #
+##################################
+
+import os, xbmcaddon, xbmcgui
+from resources.lib.tcCommon import *
+
+addonSettings = xbmcaddon.Addon( "script.thumbnailscleaner" )
+addonName     = addonSettings.getAddonInfo( "name" )
+addonVersion  = addonSettings.getAddonInfo( "version" )
+addonResource = os.path.join( addonSettings.getAddonInfo( "path" ), "resources", "lib" )
+
+dbCheck = "True"
+try: dbTest = RawXBMC.Execute( 'SELECT idVersion FROM version' )
+except:
+     dbCheck = "False"
+     xbmcgui.Dialog().ok( "%s - %s" % ( addonName, addonLanguage(32118) ),  addonLanguage(32119), addonLanguage(32120) )
+
+if dbCheck == "True":
+     xbmc.log( "[%s] - Starting %s v%s " % ( addonName, addonName, addonVersion ) )
+     xbmc.executebuiltin( 'XBMC.RunScript( %s )' % os.path.join( addonResource, "tcMain.py" ) )
