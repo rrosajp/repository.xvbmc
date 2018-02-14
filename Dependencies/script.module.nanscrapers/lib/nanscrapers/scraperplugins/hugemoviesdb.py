d8a09463b78dd63d9fb226144c92bdb86cca0d0a
EPiC-APOC <server_root@hotmail.com>
2018-02-14 09:41:21 +0100

    updates bitches... (copypaste forkers suck balls in hell with yo momma)

--- Dependencies/script.module.nanscrapers/lib/nanscrapers/scraperplugins/hugemoviesdb.py
+++ Dependencies/script.module.nanscrapers/lib/nanscrapers/scraperplugins/hugemoviesdb.py
@@ -1,35 +1,37 @@
-import requests,urlresolver
+import requests
+import resolveurl as urlresolver
 import urlparse
 import re,xbmcaddon,time 
 from ..scraper import Scraper
 from ..common import clean_title,clean_search,random_agent,send_log,error_log
 dev_log = xbmcaddon.Addon('script.module.nanscrapers').getSetting("dev_log")
   
-class solarmovie(Scraper):
-    domains = ['http://www.solarmovie.ms']
-    name = "SolarMovie"
+class hugemoviesdb(Scraper):
+    domains = ['http://hugemoviesdb.net']
+    name = "HugeMoviesDB"
     sources = []
 
     def __init__(self):
-        self.base_link = 'http://www.solarmovie.ms'
+        self.base_link = 'http://hugemoviesdb.net'
         if dev_log=='true':
             self.start_time = time.time()                                                    
 
 
     def scrape_movie(self, title, year, imdb, debrid = False):
         try:
-            search_id = clean_search(title.lower())                                                                                                                           #(movie name keeping spaces removing excess characters)
+            search_id = clean_search(title.lower())                                                                                                                           
 
-            start_url = '%s/keywords/%s' %(self.base_link,search_id.replace(' ','%20'))  
-            #print 'SolarMovie - scrape_movie - start_url:  ' + start_url           
+            start_url = '%s/?s=%s' %(self.base_link,search_id.replace(' ','+'))  
+            #print 'hugemoviedb - scrape_movie - start_url:  ' + start_url           
             
             headers={'User-Agent':random_agent()}
             html = requests.get(start_url,headers=headers,timeout=5).content     
-            
-            match = re.compile('<span class="name"><a title="(.+?)" href="(.+?)".+?title="(.+?)"',re.DOTALL).findall(html)
-            for name,item_url, year in match:
-                if year in year:                                                        
-                    if clean_title(search_id).lower() == clean_title(name).lower():  
+            match = re.compile('id="mt-.+?href="(.+?)".+?alt="(.+?)"',re.DOTALL).findall(html)
+            for item_url, name in match:
+                
+                if year in name:
+                    name_chk= name.split('(')[0]
+                    if clean_title(search_id).lower() == clean_title(name_chk).lower():  
                         self.get_source(item_url)  
             return self.sources
         except Exception, argument:        
@@ -40,21 +42,21 @@ class solarmovie(Scraper):
             
     def get_source(self,item_url):
         try:
-            #print 'solar Pass ' + item_url
+            #print 'hugemoviedb pass ' + item_url
             headers={'User-Agent':random_agent()}
             OPEN = requests.get(item_url,headers=headers,timeout=5).content
 
-            Endlinks = re.compile('id="link_.+?target="_blank" id="(.+?)"',re.DOTALL).findall(OPEN)
-            #print 'SolarMovie - scrape_movie - EndLinks: '+str(Endlinks)
+            Endlinks = re.compile('<iframe src="(.+?)"',re.DOTALL).findall(OPEN)
+            #print 'hugemoviedb - scrape_movie - EndLinks: '+str(Endlinks)
             count = 0
             for link in Endlinks:
                 if 'openload' in link:
                     try:
                         headers = {'User_Agent':User_Agent}
                         get_res=requests.get(link,headers=headers,timeout=5).content
-                        rez = re.compile('description" content="(.+?)"',re.DOTALL).findall(get_res)[0]
-                        if '1080p' in rez:
-                            qual = '1080p'
+                        rez = re.compile('<span class="N/A">(.+?)</span>',re.DOTALL).findall(get_res)[0]
+                        if '1080' in rez:
+                            qual ='1080p'
                         elif '720p' in rez:
                             qual='720p'
                         else:
@@ -67,7 +69,7 @@ class solarmovie(Scraper):
                     try:  
                         headers = {'User_Agent':User_Agent}
                         get_res=requests.get(link,headers=headers,timeout=5).content
-                        rez = re.compile('description" content="(.+?)"',re.DOTALL).findall(get_res)[0]
+                        rez = re.compile('<span class="N/A">(.+?)</span>',re.DOTALL).findall(get_res)[0]
                         if '1080' in rez:
                             qual = '1080p'
                         elif '720' in rez:
